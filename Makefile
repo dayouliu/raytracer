95b695efc54df265885e886ea129805d9d5e7c98
dayouliu <dayouliu0@gmail.com>
2023-09-03 13:11:57 -0400

    Auto commit

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 .PHONY: mytarget
 
 ppm:
+	/snap/clion/249/bin/cmake/linux/x64/bin/cmake --build /home/david/CLionProjects/raytracer/cmake-build-debug --target raytracer -j 14
 	cmake-build-debug/raytracer > cmake-build-debug/image.ppm
--- main.cpp
+++ main.cpp
@@ -30,7 +30,43 @@ void testPPM() {
     std::clog << "\rDone.                 \n";
 }
 
+// Detect if a ray will intersect a sphere
+// vector equation of ray: P(t) = O + dt
+// vector equation of sphere: r^2 = (x - C_x)^2 + (y - C_y)^2 + (z - C_z)^2 = (P_s - C) dot (P_s - C)
+// where r is radius, C is center of sphere, P_s is point on sphere
+// We plug in P_s = P(t) solve for t => if there is solution, ray will intersect the sphere at some t
+// Resulting equation is quadratic, we check the discriminant to see if there is a solution
+// negative = no roots => no intersection
+// 0 or positive = at least 1 root => intersection
+// we return t, if t < 0 => no intersection (or intersection is at back of camera)
+double ray_sphere_intersect(const ray &r, const point3 &center, double radius) {
+    point3 O = r.origin();
+    vec3 d = r.direction();
+    vec3 oc = O - center;
+    // equation for a, b,c in quadratic formula
+    double a = dot(d, d);
+    double b = dot(2*d, oc);
+    double c = dot(oc, oc) - radius*radius;
+    double discriminant = b*b - 4*a*c;
+
+    // Does not intersect sphere
+    if(discriminant < 0) {
+        return -1;
+    }
+    return (-b - std::sqrt(discriminant)) / (2.0 * a);
+}
+
 color ray_color(const ray &r) {
+    const vec3 sphere_center = vec3(0, 0, -1);
+    const double sphere_radius = 0.5;
+    const double t_intersect = ray_sphere_intersect(r, sphere_center, sphere_radius);
+    if(t_intersect > 0.0) {
+        const vec3 n = unit_vector(r.at(t_intersect) - sphere_center); // normal
+        // Transform normal values from domain [-1, 1] => [0, 1] for color
+        const color n_color = color(n.x()+1, n.y()+1, n.z()+1) * 0.5;
+        return n_color;
+    }
+
     vec3 unit_dir = unit_vector(r.direction());
     // unit_ray_vec.y => 0.5
     double a = 0.5 * (unit_dir.y() + 1.0);
@@ -79,5 +115,6 @@ int main() {
             write_color(std::cout, pixel_color);
         }
     }
+
     std::clog << "\rDone.                 \n";
 }
